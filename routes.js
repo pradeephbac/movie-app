4f7eb5a0c8c3342150284f935738ee3f381d66b5
NilupulL <nilupull@99x.lk>
2018-06-07 14:15:26 +0530

    changing port to get heroku port from environment

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app);
-
-server.listen(8080);
+var port = process.env.PORT || 8000
+server.listen(port, function () {
+    console.log("App is running on port " + port);
+});
 app.use(express.static("./"));
 app.use("/node_modules", express.static('node_modules'));
 app.use("/public", express.static('public'));
