5adeb40f8c418504a60cd03fb1796e12cd870bde
NilupulL <nilupull@99x.lk>
2018-06-11 09:08:18 +0530

    Adding api endpoints and configuring database

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app);
-var port = process.env.PORT || 8000
+    server = require('http').createServer(app),
+    _ = require('lodash'),
+    MongoClient = require('mongodb').MongoClient,
+    url = "mongodb://admin:test1234@ds255320.mlab.com:55320/yamovies";
+
+
+var port = process.env.PORT || 8080
 server.listen(port, function () {
     console.log("App is running on port " + port);
 });
@@ -9,6 +14,41 @@ app.use(express.static("./"));
 app.use("/node_modules", express.static('node_modules'));
 app.use("/public", express.static('public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/getAllMovies', function (req, res) {
+    MongoClient.connect(url, function (err, client) {
+        var db = client.db('yamovies');
+        db.collection('movies').find({}).toArray()
+            .then((results) => {
+                res.send(results);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    });
+});
+
+app.get('/getMovies', function (req, res) {
+    MongoClient.connect(url, function (err, client) {
+        var db = client.db('yamovies');
+        db.collection('movies').find({"is_main":false}).toArray()
+            .then((results) => {
+                res.send(results);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    });
+});
+
+app.get('/getMainMovies', function (req, res) {
+    MongoClient.connect(url, function (err, client) {
+        var db = client.db('yamovies');
+        db.collection('movies').find({"is_main":true}).toArray()
+            .then((results) => {
+                res.send(results);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    });
 });
